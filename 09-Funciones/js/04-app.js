90a249d1b685bffc9a054ee640cb7fd9a0fdbb26
Jhafet19 <20223tn107@utez.edu.mx>
2023-09-17 10:27:01 -0600

    Mas funciones

---
+++ 09-Funciones/js/04-app.js
@@ -0,0 +1,15 @@
+/**Diferencias entre metodos y funciones */
+
+const numero1=20;
+const numero2="20";
+
+console.log(parseInt(numero2))//Esto es un afuncion
+
+console.log(numero1.toString())//Esto es un metodo
+
+function sumar(){
+    console.log(2+2)
+
+}
+
+sumar()
---
+++ 09-Funciones/js/05-app.js
@@ -0,0 +1,13 @@
+function sumar(a,b){
+console.log(a+b)
+}
+/**
+ * 12 y 3 son argumentos
+ */
+sumar(200,300);
+sumar(200,2);
+
+function saludar(nombre,apellido){
+    console.log(`Hola ${nombre} ${apellido}`);
+}
+saludar("jhafet","Rodriguez")
