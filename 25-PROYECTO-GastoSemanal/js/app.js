62d43972dbdcadd6715ad3fa3a81613a3eb9568a
Jhafet19 <20223tn107@utez.edu.mx>
2023-12-20 15:15:51 -0600

    2 proyectos como de que no

--- 25-PROYECTO-GastoSemanal/js/app.js
+++ 25-PROYECTO-GastoSemanal/js/app.js
@@ -18,14 +18,19 @@ class Presupuesto {
     }
     nuevoGasto(gasto) {
         this.gastos = [...this.gastos, gasto]
-        console.log(this.gastos);
+        this.calcularRestante();
+    }
+    calcularRestante() {
+        const gastado = this.gastos.reduce((total, gasto) => total + gasto.cantidad, 0)
+        this.restante = this.presupuesto - gastado
+
+        console.log(this.restante);
     }
 }
 
 class UI {
     insertarPresupuesto(cantidad) {
         const { presupuesto, restante } = cantidad
-        console.log(document.querySelector('#total'));
         document.querySelector('#total').textContent = presupuesto
         document.querySelector('#restante').textContent = restante
     }
@@ -47,6 +52,55 @@ class UI {
             divAlerta.remove()
         }, 3000)
     }
+
+    agregarGastoListado(gastos) {
+        this.limpiarHtml();
+
+        gastos.forEach(gasto => {
+
+            const { cantidad, id, nombre } = gasto;
+
+            const nuevoGasto = document.createElement('li');
+            nuevoGasto.className = 'list-group-item d-flex justify-content-between aling-items-center'
+            nuevoGasto.dataset.id = id;
+
+            nuevoGasto.innerHTML = `${nombre} <span class="badge badge-primary badge-pill"> ${cantidad}</span>`
+
+
+            const btnBorrar = document.createElement('button');
+            btnBorrar.classList.add('btn', 'btn-danger', 'borrar-gasto')
+            btnBorrar.innerHTML = 'borrar &times'
+            nuevoGasto.appendChild(btnBorrar)
+
+            gastoListado.appendChild(nuevoGasto)
+        });
+    }
+
+    actualizarRestante(restante) {
+        document.querySelector('#restante').textContent = restante
+
+    }
+
+    comprobarPresupuesto(presupuestoOj) {
+        const { presupuesto, restante } = presupuestoOj
+        const restanteDiv = document.querySelector('.restante')
+        if ((presupuesto / 4) > restante) {
+            restanteDiv.classList.remove('alert-succes')
+
+            restanteDiv.classList.add('alert-danger')
+        }else if((presupuesto/2)>restante){
+            restanteDiv.classList.remove('alert-succes')
+
+            restanteDiv.classList.add('alert-warning')
+        }
+
+    }
+
+    limpiarHtml() {
+        while (gastoListado.firstChild) {
+            gastoListado.removeChild(gastoListado.firstChild)
+        }
+    }
 }
 
 const ui = new UI();
@@ -62,7 +116,6 @@ function preguntarPresupuesto() {
     }
 
     presupuesto = new Presupuesto(presupuestoUsuario)
-    console.log(presupuesto);
 
     ui.insertarPresupuesto(presupuesto)
 
@@ -74,7 +127,6 @@ function agregarGasto(e) {
     const cantidad = Number(document.querySelector('#cantidad').value);
 
     if (nombre === '' || cantidad === '') {
-        console.log("ambos son obligatirios");
         ui.alerta('ambos campos son obligatorios', 'error');
     } else if (cantidad <= 0 || isNaN(cantidad)) {
         ui.alerta('cantidad no valida', 'error')
@@ -84,9 +136,16 @@ function agregarGasto(e) {
     const gasto = {
         nombre, cantidad, id: Date.now()
     }
+    const { gastos, restante } = presupuesto
+
+    ui.agregarGastoListado(gastos)
 
     presupuesto.nuevoGasto(gasto)
-    ui.alerta('Gasto agregado correctamente','')
+    ui.alerta('Gasto agregado correctamente', '')
+    ui.actualizarRestante(restante)
+    ui.comprobarPresupuesto(presupuesto);
     formulario.reset();
+
+
 }
 
