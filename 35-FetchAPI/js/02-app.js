9062ef35a6b9beed33546f539cc92f2a3ef38f3c
jhafet19 <20223tn107@utez.edu.mx>
2024-10-09 10:25:41 -0600

    asd

--- 35-FetchAPI/js/02-app.js
+++ 35-FetchAPI/js/02-app.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const btnCargarJson = document.querySelector("#cargarJSON");
 const url = "data/empleado.json";
 btnCargarJson.addEventListener("click", obtenerInfo);
@@ -13,3 +14,21 @@ function obtenerInfo() {
     })
     .catch((err) => console.log(err));
 }
+=======
+const jsonBtn = document.querySelector('#cargarJSON')
+jsonBtn.addEventListener('click', obtenerJson)
+
+function obtenerJson() {
+    const url = 'data/empleado.json'
+
+    fetch(url).then(res => {
+        if (res.status) {
+            return res.json()
+        }
+    }).then(data => {
+        console.log(data)
+    }).catch(err => {
+        console.log(err)
+    })
+}
+>>>>>>> Stashed changes
