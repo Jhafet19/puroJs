af07c4c5834cb7aaa52a1dbf5c4db7e2b3c0563e
Jhafet19 <20223tn107@utez.edu.mx>
2026-06-13 18:42:06 -0600

    53 inicia proyecto con express y Node

---
+++ 53-agenciaViajesNode/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import router from "./routes/index.js";
+
+
+const app = express()
+const port = process.env.PORT || 4000
+
+app.use('/', router)
+
+app.set('view engine', 'pug')
+
+app.listen(port, () => {
+    console.log('El servidor esta escuchando el puerto ', port)
+})
---
+++ 53-agenciaViajesNode/routes/index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.render('inicio')
+})
+router.get('/nosotros', (req, res) => {
+    const viajes = 'Viaje a Alemania'
+
+    res.render('nosotros', {
+        viajes
+    })
+})
+router.get('/contacto', (req, res) => {
+    res.send('HOLIS desde contacto')
+
+})
+
+export default router
