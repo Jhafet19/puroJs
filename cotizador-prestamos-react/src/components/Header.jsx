8a8025a0c01b0670642d590fa1e2549b2a2d2bb7
JhafetGit <jonargj04@gmail.com>
2026-06-28 17:18:40 -0600

    Nuevo proyecto

---
+++ cotizador-prestamos-react/src/components/Header.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Header() {
+    return (
+        <h1 className="text-4xl font-extrabold text-gray-500 text-center">
+            ¿Cuanto <span className="text-indigo-600">Dinero </span> Necesitas?
+        </h1>
+    )
+}
