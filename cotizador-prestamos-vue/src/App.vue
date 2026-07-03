54831f9aeb175b8867383acca5118008af2a8f1b
JhafetGit <jonargj04@gmail.com>
2026-07-02 20:29:02 -0600

    Se agrega funcionalidad de incremento y decremento por pasos de 100

--- cotizador-prestamos-vue/src/App.vue
+++ cotizador-prestamos-vue/src/App.vue
@@ -1,24 +1,59 @@
 <script setup>
-import { reactive, ref } from 'vue';
+import { computed, reactive, ref } from 'vue';
 import Header from './components/Header.vue';
 
-const cantidad = ref(0)
+const cantidad = ref(10000)
 const state = reactive({ cantidad: 0 })
+const MIN = 2000;
+const MAX = 20000
+const STEP = 100
+const formatearDiner = computed(() => {
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  });
+  return formatter.format(cantidad.value)
+})
 
-function handleChange(e) {
-  console.log()
-  cantidad.value = Number(e.target.value)
+const handleChangeDecremento = () => {
+  const valor = cantidad.value - STEP;
+  if (valor < MIN) {
+    alert('La cantidad no es valida')
+    return
 
+  }
+  cantidad.value = valor
 }
+
+
+const handleChangeIncremento = () => {
+  const valor = cantidad.value + STEP;
+  if (valor > MAX) {
+    alert('La cantidad no es valida')
+    return
+  }
+  cantidad.value = valor
+}
+
 </script>
 
 <template>
   <div class="my-20 max-w-lg mx-auto  bg-white shadow p-10">
     <Header />
+    <div class="flex justify-between mt-10">
+      <button class="h-10 w-10 flex items-center justify-center font-bold bg-lime-500 rounded-full 
+        hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-lime-500 text-white text-2xl"
+        @click="handleChangeDecremento">-</button>
+
+      <button class="h-10 w-10 flex items-center justify-center font-bold bg-lime-500 rounded-full 
+        hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-lime-500 text-white text-2xl"
+        @click="handleChangeIncremento">+</button>
+    </div>
     <div class="my-5">
-      <input type="range" class="w-full bg-gray-200 accent-lime-500 hover:accent-lime-600 " @:input="handleChange" />
-      <p>$ {{ cantidad }}</p>
-      
+      <input type="range" :min=MIN :max=MAX :step=STEP class="w-full bg-gray-200 accent-lime-500 hover:accent-lime-600 "
+        v-model.number="cantidad" />
+      <p class="text-center my-10 text-5xl font-extrabold text-indigo-600">{{ formatearDiner }}</p>
+
 
     </div>
   </div>
