04079b8fc9cf8dedd0c77a61780e40faf143e97c
JhafetGit <jonargj04@gmail.com>
2026-07-09 19:42:31 -0600

    se crean tokens dinamicos y se empieza el proceso de confirmar

--- fullstackJS/backend/controllers/veterinarioController.js
+++ fullstackJS/backend/controllers/veterinarioController.js
@@ -2,7 +2,14 @@ import Veterinario from "../models/veterinario.js"
 
 const registrar = async (req, res) => {
 
-    const { email, password, nombre } = req.body
+    const { email } = req.body
+    const existeUsuario = await Veterinario.findOne({ email })
+    if (existeUsuario) {
+        console.log('Existe usuario')
+        const error = new Error('Usuario ya registrado')
+        return res.status(400).json({ message: error.message })
+    }
+
     try {
         const veterinario = new Veterinario(req.body)
         const veterinarioGuardado = await veterinario.save()
@@ -20,7 +27,13 @@ const perfil = (req, res) => {
     res.json({ url: 'Desde APi/Veterinarios/Perfil' })
 }
 
+const confirmar = (req, res) => {
+    console.log(req.params.token)
+    res.json({ msg: 'Confirmando Cuenta' })
+}
+
 export {
     registrar,
-    perfil
+    perfil,
+    confirmar
 }
