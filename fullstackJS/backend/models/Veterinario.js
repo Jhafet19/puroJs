e073b803edf6a21f38cfbb6a4741883d0042be19
JhafetGit <jonargj04@gmail.com>
2026-07-14 19:15:25 -0600

    termina funcionalidades de Veterinario

--- fullstackJS/backend/models/Veterinario.js
+++ fullstackJS/backend/models/Veterinario.js
@@ -40,7 +40,7 @@ const veterinarioSchema = mongoose.Schema({
 
 veterinarioSchema.pre('save', async function () {
     if (!this.isModified('password')) {
-        next()
+        return
     }
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
