4b508cf58adaabf0a6e3b4eb2f671f76281dd24e
JhafetGit <jonargj04@gmail.com>
2026-07-21 18:57:51 -0600

    Se hacen formularios de login y Registrar

--- fullstackJS/frontend/src/pages/Login.jsx
+++ fullstackJS/frontend/src/pages/Login.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 
 
 export default function Login() {
@@ -6,7 +8,23 @@ export default function Login() {
             <div>
                 <h1 className=" text-indigo-600 font-black text-6xl">Inicia Sesión y Administra Tus <span className="text-black"> Pacientes</span> </h1 >
             </div >
-            <div>2</div>
+            <div>
+                <form action="" className="mt-20 md:mt-5 shadow-lg p-3  px5 py-10 rounded-xl bg-white">
+                    <div className="my-5">
+                        <label htmlFor="" className=" uppercase text-gray-600  block text-xl font-bold">Email</label>
+                        <input type="email" name="" id="" className=" border w-full p-3 mt-3 bg-gray-50 rounded-xl" placeholder="Email de registro" />
+                    </div>
+                    <div className="my-5">
+                        <label htmlFor="" className=" uppercase text-gray-600  block text-xl font-bold">Password</label>
+                        <input type="password" name="" id="" className=" border w-full p-3 mt-3 bg-gray-50 rounded-xl" placeholder="Password de registro" />
+                    </div>
+                    <input type="submit" value='Iniciar Sesión' className="rounded-xl md:w-auto bg-indigo-700 w-full py-2 text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800" />
+                </form>
+                <nav className="mt-10 lg:flex lg:justify-between ">
+                    <Link className="block text-center my-5  text-gray-500" to="/registrar">¿No tienes una cuenta?, Crea una </Link>
+                    <Link className="block text-center my-5  text-gray-500" to="/olvide-password">Olvide mi Password</Link>
+                </nav>
+            </div>
 
         </>
     )
--- fullstackJS/frontend/src/pages/Registrar.jsx
+++ fullstackJS/frontend/src/pages/Registrar.jsx
@@ -1,9 +1,37 @@
 
+import { Link } from "react-router-dom"
+
 export default function Registrar() {
     return (
         < >
-            <h1>Registrar</h1>
-
+            <div>
+                <h1 className=" text-indigo-600 font-black text-6xl">Crea tu Cuenta y Administra{' '}  <span className="text-black"> Tus Pacientes </span> </h1 >
+            </div >
+            <div>
+                <form action="" className="mt-20 md:mt-5 shadow-lg p-3  px5 py-10 rounded-xl bg-white">
+                    <div className="my-5">
+                        <label htmlFor="" className=" uppercase text-gray-600  block text-xl font-bold">Nombre</label>
+                        <input type="text" name="" id="" className=" border w-full p-3 mt-3 bg-gray-50 rounded-xl" placeholder="Tu Nombres" />
+                    </div>
+                    <div className="my-5">
+                        <label htmlFor="" className=" uppercase text-gray-600  block text-xl font-bold">Email</label>
+                        <input type="email" name="" id="" className=" border w-full p-3 mt-3 bg-gray-50 rounded-xl" placeholder="Email de registro" />
+                    </div>
+                    <div className="my-5">
+                        <label htmlFor="" className=" uppercase text-gray-600  block text-xl font-bold">Password</label>
+                        <input type="password" name="" id="" className=" border w-full p-3 mt-3 bg-gray-50 rounded-xl" placeholder="Tu password" />
+                    </div>
+                    <div className="my-5">
+                        <label htmlFor="" className=" uppercase text-gray-600  block text-xl font-bold">Repetir Password</label>
+                        <input type="password" name="" id="" className=" border w-full p-3 mt-3 bg-gray-50 rounded-xl" placeholder="repite tu password" />
+                    </div>
+                    <input type="submit" value='Iniciar Sesión' className="rounded-xl md:w-auto bg-indigo-700 w-full py-2 text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800" />
+                </form>
+                <nav className="mt-10 lg:flex lg:justify-between ">
+                    <Link className="block text-center my-5  text-gray-500" to="/">¿Ya tienes una cuenta? Inicia Sesión </Link>
+                    <Link className="block text-center my-5  text-gray-500" to="/olvide-password">Olvide mi Password</Link>
+                </nav>
+            </div>
         </>
     )
 }
