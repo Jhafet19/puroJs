26141287f18272a0a51b2e6d50e559bff2fb0793
JhafetGit <jonargj04@gmail.com>
2026-08-20 19:15:50 -0600

    uso de openRouter

--- nucleus-js-ia/main.js
+++ nucleus-js-ia/main.js
@@ -1,1 +1,30 @@
 import './style.css'
+import { createOpenRouter } from '@openrouter/ai-sdk-provider'
+import { generateText, streamText } from 'ai'
+
+const openRoute = createOpenRouter({
+    api_keys=import.meta.env.VITE_OPENROUTER_KEY
+})
+
+const form = document.querySelector('#form')
+
+form.addEventListener('submit', async e => {
+    e.preventDefault();
+
+    const pront = document.querySelector('#pront').value
+    console.log("🚀 ~ pront:", pront)
+    if (pront.trim() === '') alert('La consulta no puede ir vacia ')
+
+    const result = streamText({
+        model: openRoute('google/gemini-2.5-pro-exp-03-25 : free'),
+        // model: openRoute('deepseek/deepseek-chat- v3-0324 : free'),
+        // model: openRoute('qwen/qwen2.5-vI-32b-instruct : free'),
+
+        prompt
+    })
+
+    for await (const text of result.textStream) {
+        console.log(text)
+    }
+
+})
